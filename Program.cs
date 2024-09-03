66170326456ec406a0396907db82eb8044f259da
FLP013 <felliperibeiro9@gmail.com>
2024-09-02 21:12:46 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("|"); 
+Console.Write("|    "); Console.Write("|");Console.Write("|");
+Console.WriteLine();
+
+Console.Write(@"\");Console.Write(@"\"); Console.Write(" ()"); Console.Write(@" /");Console.Write(@"/");
+
+Console.WriteLine();
+
+Console.Write(@"/");Console.Write(@"/");Console.Write("(__)");Console.Write(@"\");Console.Write(@"\");
+
+Console.WriteLine();
+
+Console.Write("|");
+Console.Write("|    ");Console.Write("|");Console.Write("|");
